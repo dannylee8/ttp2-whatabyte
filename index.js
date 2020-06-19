1d90ad336841c246232ad2f7e6eea4696967e361
Danny Lee <dreamer@panix.com>
2020-06-19 00:55:32 -0400

    secure user route

--- index.js
+++ index.js
@@ -85,6 +85,12 @@ passport.deserializeUser((user, done) => {
   done(null, user);
 });
 
+// Creating custom middleware with Express
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
 // Router mounting
 app.use("/", authRouter);
 
@@ -92,12 +98,24 @@ app.use("/", authRouter);
  * Routes Definitions
  */
 
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
+
 app.get("/", (req, res) => {
   res.render("index", { title: "Home" });
 });
 
-app.get("/user", (req, res) => {
-  res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+app.get("/user", secured, (req, res, next) => {
+  const { _raw, _json, ...userProfile } = req.user;
+  res.render("user", {
+    title: "Profile",
+    userProfile: userProfile,
+  });
 });
 
 /**
